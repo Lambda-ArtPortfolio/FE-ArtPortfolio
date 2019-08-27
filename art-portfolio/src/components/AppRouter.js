e4e8cd866101768f196df6b29b169d42bcbbc45c
Alexis Panyathong <alexispanyathong@gmail.com>
2019-08-26 23:44:33 -0500

    Login Updates
    I took out the formik and used initialState and reducers instead. I have the login form built back up again and did some styling for practice and easier to look at. I need to talk with the back-end person again because the username and password that he gave me doesn't work anymore.

--- art-portfolio/src/components/AppRouter.js
+++ art-portfolio/src/components/AppRouter.js
@@ -1,19 +1,23 @@
+//* Alexis Panyathong's Section *//
+
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 import Login from './Login';
 
+
+//Styled Components
+import { Nav } from './StyledWidgets';
+
 const AppRouter = () => {
 
     return (
         <Router>
             <div className="app-router">
-                <ul>
-                    <li>
-                        <Link to='/login'>Login</Link>
-                    </li>
-                </ul>
+                <Nav>
+                    <Link to='/login'>Login</Link>
+                </Nav>
             </div>
 
             <Switch>
